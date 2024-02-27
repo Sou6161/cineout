623812324a389a57c9d3fafeb77eebe0dfad50ac
Sourabh Saini <sourabh61saini@gmail.com>
2024-02-27 19:51:34 +0530

    code updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import { useFormik } from 'formik';
 
 const Login = () => {
-  // Note that we have to initialize ALL of fields with values. These
-  // could come from props, but since we don’t want to prefill this form,
-  // we just use an empty string. If we don’t do this, React will yell
-  // at us.
+  
   const formik = useFormik({
     initialValues: {
       firstName: '',
