a0932c16b4eeff3392c177727b6cb6b3ab69363d
sourabh saini <sourabh61saini@gmail.com>
2024-07-11 13:37:56 +0530

    Changes Updated

--- src/components/Nowshowingdata.jsx
+++ src/components/Nowshowingdata.jsx
@@ -13,11 +13,11 @@ const useWindowSize = () => {
     const handleResize = () => {
       setSize([window.innerWidth, window.innerHeight]);
     };
-    
-    window.addEventListener('resize', handleResize);
-    
+
+    window.addEventListener("resize", handleResize);
+
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -55,19 +55,26 @@ const Nowshowingdata = ({ nowfinal }) => {
       backgroundRepeat: "no-repeat",
     };
 
-    if (width >= 1536) { // 2xl
-      setBackgroundStyle({ ...baseStyle, backgroundPosition: "-30vh 0vw" });
-    } else if (width >= 1280) { // xl
-      setBackgroundStyle({ ...baseStyle, backgroundPosition: "-25vh 0vw" });
-    } else if (width >= 1024) { // lg
-      setBackgroundStyle({ ...baseStyle, backgroundPosition: "-20vh 0vw" });
-    } else if (width >= 768) { // md
+    if (width >= 1536) {
+      // 2xl
       setBackgroundStyle({ ...baseStyle, backgroundPosition: "-0vh 0vw" });
-    } else if (width >= 640) { // sm
+    } else if (width >= 1280) {
+      // xl
       setBackgroundStyle({ ...baseStyle, backgroundPosition: "-0vh 0vw" });
-    } else if (width >= 480) { // xsmall
+    } else if (width >= 1024) {
+      // lg
       setBackgroundStyle({ ...baseStyle, backgroundPosition: "-0vh 0vw" });
-    } else { // smaller than xsmall
+    } else if (width >= 768) {
+      // md
+      setBackgroundStyle({ ...baseStyle, backgroundPosition: "-0vh 0vw" });
+    } else if (width >= 640) {
+      // sm
+      setBackgroundStyle({ ...baseStyle, backgroundPosition: "-0vh 0vw" });
+    } else if (width >= 480) {
+      // xsmall
+      setBackgroundStyle({ ...baseStyle, backgroundPosition: "-0vh 0vw" });
+    } else {
+      // smaller than xsmall
       setBackgroundStyle({ ...baseStyle, backgroundPosition: "center" });
     }
   }, [width, bannerUrl]);
@@ -88,54 +95,80 @@ const Nowshowingdata = ({ nowfinal }) => {
         <Header />
       </div>
       <div
-        className="w-[100vw] h-[45vh] xsmall:h-[47vh] small:h-[52vh] medium:h-[56vh] relative"
+        className="w-[100vw] h-[45vh] xsmall:h-[47vh] small:h-[52vh] medium:h-[56vh] large:h-[60vh] xlarge:h-[65vh] 2xlarge:h-[70vh]  relative"
         style={backgroundStyle}
       >
-      
-          <h1
-            className="text-[5vw] font-bold relative top-[22vh] left-[2vh]  xsmall:top-[25vh] small:top-[30vh] medium:top-[33vh] mb-2"
-            style={titleStyle}
-          >
-            {nowfinal.name ||
-              nowfinal.title ||
-              nowfinal.original_name ||
-              nowfinal.original_title}
-          </h1>
+        <h1
+          className="text-[5vw] font-bold relative top-[23vh] left-[2vh]  xsmall:top-[25vh] small:top-[30vh] medium:top-[33vh] medium:text-[3vw] large:top-[36vh] large:text-[3vw] xlarge:text-[3vw] xlarge:top-[40vh] 2xlarge:top-[42vh] 2xlarge:text-[2.5vw] mb-2"
+          style={titleStyle}
+        >
+          {nowfinal.name ||
+            nowfinal.title ||
+            nowfinal.original_name ||
+            nowfinal.original_title}
+        </h1>
 
+        <p
+          className="text-sm font-semibold max-w-[60vw] mb-2 relative top-[23vh] xsmall:top-[25vh] small:top-[30vh] medium:top-[33vh] large:text-[1.5vw] large:top-[37vh] xlarge:text-[1.3vw] xlarge:top-[40vh] 2xlarge:top-[43vh] 2xlarge:text-[1.1vw] left-[2vh]"
+          style={detailsStyle}
+        >
+          {width >= 1536 ? ( // 2xlarge
+            <>
+              {nowfinal.overview.slice(0, 180)}
+              {nowfinal.overview.length > 180 && "..."}
+            </>
+          ) : width >= 1280 ? ( // xlarge
+            <>
+              {nowfinal.overview.slice(0, 200)}
+              {nowfinal.overview.length > 200 && "..."}
+            </>
+          ) : width >= 1024 ? ( // large
+            <>
+              {nowfinal.overview.slice(0, 170)}
+              {nowfinal.overview.length > 170 && "..."}
+            </>
+          ) : width >= 768 ? ( // medium
+            <>
+              {nowfinal.overview.slice(0, 150)}
+              {nowfinal.overview.length > 150 && "..."}
+            </>
+          ) : (
+            <>
+              {nowfinal.overview.slice(0, 80)}
+              {nowfinal.overview.length > 100 && "..."}
+            </>
+          )}
+          <Link
+            to={`/title/tt${nowfinal.id}`}
+            className="text-blue-500 hover:underline ml-1"
+            style={{ textShadow: "none" }}
+          >
+            more
+          </Link>
+        </p>
+
+        <div className="relative top-[23vh] left-[2vh] xsmall:top-[25vh] small:top-[30vh] medium:top-[34vh] large:top-[38vh] xlarge:top-[42vh]  2xlarge:top-[44vh]">
           <p
-            className="text-sm font-semibold max-w-[60vw] mb-2 relative top-[22vh] xsmall:top-[25vh] small:top-[30vh] medium:top-[33vh] left-[2vh]"
+            className="text-sm 2xlarge:text-[1vw] xlarge:text-[1.3vw] font-semibold flex gap-2 items-center"
             style={detailsStyle}
           >
-            {nowfinal.overview.slice(0, 100)}....
-            <Link
-              to={`/title/tt${nowfinal.id}`}
-              className="text-blue-500 hover:underline ml-1"
-              style={{ textShadow: "none" }}
-            >
-              more
-            </Link>
+            <GrAnnounce className="text-yellow-400 text-xl 2xlarge:text-[2vw] xlarge:text-[1.6vw]" />{" "}
+            {nowfinal.first_air_date ||
+              nowfinal.release_date ||
+              "No Information"}
+            <FaFire className="text-orange-500 text-xl xlarge:text-[1.6vw] 2xlarge:text-[2vw]" />
+            {nowfinal.popularity || "No Information"}
           </p>
+        </div>
 
-          <div className="relative top-[22vh] left-[2vh] xsmall:top-[25vh] small:top-[30vh] medium:top-[34vh] ">
-            <p className="text-sm font-semibold flex gap-2 items-center" style={detailsStyle}>
-              <GrAnnounce className="text-yellow-400 text-xl" />{" "}
-              {nowfinal.first_air_date ||
-                nowfinal.release_date ||
-                "No Information"}
-              <FaFire className="text-orange-500 text-xl" />
-              {nowfinal.popularity || "No Information"}
-            </p>
-          </div>
-
-          <div className=" relative xsmall:top-[21vh] xsmall:left-[65vw] small:top-[25vh] small:left-[72vw] medium:top-[25vw] medium:left-[77vw] ">
-            <Link
-              to={`/title/tt${nowfinal.id}`}
-              className="comic-button inline-block"
-            >
-              <button>Watch Trailer</button>
-            </Link>
-          </div>
-        
+        <div className=" relative xsmall:top-[23vh] xsmall:left-[77vw] small:top-[27vh] small:left-[77vw] medium:top-[32vh] medium:left-[84vw] large:top-[32vh] large:left-[88vw] xlarge:top-[35vh] xlarge:left-[88vw] 2xlarge:top-[40vh] 2xlarge:left-[90vw] ">
+          <Link
+            to={`/title/tt${nowfinal.id}`}
+            className="comic-button inline-block"
+          >
+            <button>Watch Trailer</button>
+          </Link>
+        </div>
       </div>
     </>
   );
--- src/components/Upcomingdata.jsx
+++ src/components/Upcomingdata.jsx
@@ -9,16 +9,16 @@ const Upcomingdata = ({
 }) => {
   return (
     <>
-      <div className="  scrollbar-hide overflow-clip p-5 h-[67vh] ">
-        <div className=" mb-10 flex items-center gap-[47%] overflow-y-clip">
-          <h1 className=" text-3xl ml-3 font-semibold text-red-700">
+      <div className="  scrollbar-hide overflow-clip p-5 mb-5 h-[67vh] bg-red-40 ">
+        <div className=" mb-4  inline-flex items-center small:h-[5vw]  bg-red-20  overflow-y-clip">
+          <h1 className=" text-xl small:text-[3.5vw]  medium:text-[3vw] large:text-[2.5vw] xlarge:text-[2vw] 2xlarge:text-[1.5vw] ml-3  font-semibold text-red-700">
             Now Showing Movies
           </h1>
           {/* <Dropdown title="filter" options={["tv","movie","all"]}/> */}
         </div>
 
-        <div className=" w-[97vw] h-[55vh]  scrollbar-hide overflow-x-auto overflow-y-hidden  mb-[8vh] -mt-2 bg-yellow-20">
-          <div className=" ml-3 flex gap-10  w-[100vw] h-[47vh]  no-scrollbar ">
+        <div className=" w-[97vw] min-h-[58vh] 2xlarge:w-[97vw]  scrollbar-hide overflow-x-auto overflow-y-hidden -mt-6  bg-yellow-20">
+          <div className=" ml-1 flex  gap-10  w-[100vw] h-[47vh] no-scrollbar ">
             {/* {nowupcomingmovies &&console.log(nowupcomingmovies, "now upcoming movies ID")} */}
             {/* {console.log(nowupcomingfinal, "now upcoming final")} */}
             {nowupcomingfinal &&
@@ -27,9 +27,9 @@ const Upcomingdata = ({
                 return (
                   <div key={index}>
                     <Link to={`/name/nm${movies.id}`}>
-                      <div className="mr-4 ml-3 min-w-[15vw] mt-5 glow3 max-h-[42vh] rounded-[10px] p-2 overflow-x-hidden scrollbar-hide cursor-pointer bg-zinc-70 bg-slate-40 overflow-y-auto">
+                      <div className="mr-4 ml-3 max-w-[52vw] max-h-[42vh] xsmall:max-w-[40vw] small:max-w-[30vw] medium:max-w-[30vw] large:max-w-[25vw] xlarge:max-w-[20vw] 2xlarge:max-w-[15vw] mt-5 glow3  rounded-[10px] p-2 overflow-x-hidden scrollbar-hide cursor-pointer bg-zinc-70 bg-slate-40 overflow-y-hidden">
                         <img
-                          className="w-[30vw] h-[40vh] rounded-md drop-shadow-glow"
+                          className="min-w-[48vw]  h-[40vh] xsmall:min-w-[34vw] small:min-w-[25vw]  medium:min-w-[22vw] large:min-w-[18vw] xlarge:min-w-[15vw] 2xlarge:min-w-[14vw] rounded-md drop-shadow-glow"
                           src={`https://image.tmdb.org/t/p/original/${movies.poster_path}`}
                           alt="no image available"
                           onError={(e) => {
@@ -40,7 +40,7 @@ const Upcomingdata = ({
                         />
                       </div>
                       <div className=" ">
-                        <h1 className="text-2xl font-semibold hover:underline hover:cursor-pointer text-yellow-500 ml-4 mt-5 ">
+                        <h1 className="text-[5vw] xsmall:text-[4vw] small:text-[3vw] medium:text-[2vw] large:text-[1.7vw] xlarge:text-[1.5vw] 2xlarge:text-[1.3vw] font-semibold hover:underline hover:cursor-pointer text-yellow-500 ml-4 mt-5 ">
                           {movie.title}
                         </h1>
                       </div>
@@ -51,15 +51,15 @@ const Upcomingdata = ({
           </div>
         </div>
       </div>
-      <div className=" w-[100vw] h-[58vh] bg-red-30">
-        <div className=" mb-10 flex items-center gap-[47%] overflow-y-clip">
-          <h1 className=" text-3xl ml-7 font-semibold text-red-700">
+      <div className=" w-[100vw] h-[67vh]">
+        <div className=" mb-5 flex items-center gap-[47%]">
+          <h1 className=" text-xl small:text-[3.5vw]  medium:text-[3vw] large:text-[2.5vw] xlarge:text-[2vw] 2xlarge:text-[1.5vw] ml-7 font-semibold text-red-700">
             Upcoming Movies
           </h1>
         </div>
 
-        <div className=" w-[97vw] mx-4  h-[55vh]  scrollbar-hide overflow-x-auto overflow-y-hidden  mb-[8vh] -mt-2 bg-yellow-20">
-          <div className=" ml-3 flex gap-10  w-[100vw] h-[47vh]  no-scrollbar ">
+        <div className=" w-[97vw] mx-4  min-h-[60vh]  scrollbar-hide overflow-x-auto overflow-y-hidd">
+          <div className=" ml-2 flex gap-10  w-[100vw] h-[47vh]  no-scrollbar ">
             {/* {nowupcomingmovies && console.log(nowupcomingmovies," UpcomingMoviesIMDBID Final")} */}
             {/* {nowupcomingmoviesdetails && console.log(nowupcomingmoviesdetails," UpcomingMoviesDetails Final")} */}
 
@@ -70,9 +70,9 @@ const Upcomingdata = ({
                   return (
                     <Link to={`/name/nm${movie2.id}`}>
                     <div key={index}>
-                      <div className="  mr-4  ml-6 mt-4 min-w-[15vw] glow3 max-h-[42vh] rounded-[10px] p-2 overflow-x-hidden scrollbar-hide cursor-pointer bg-zinc-70  bg-slate-400  hover:bg-slate-500 overflow-y-auto">
+                      <div className="mr-4  ml-2 mt-5 max-w-[52vw] max-h-[42vh] xsmall:max-w-[40vw] small:max-w-[30vw] medium:max-w-[30vw] large:max-w-[25vw] xlarge:max-w-[20vw] 2xlarge:max-w-[15vw] overflow-y-hidden glow3 rounded-[10px] p-2 overflow-x-hidden scrollbar-hide cursor-pointer bg-zinc-70  bg-black  hover:bg-slate-500">
                         <img
-                          className=" w-[30vw] h-[40vh] rounded-md drop-shadow-glow"
+                          className="min-w-[48vw]  h-[40vh] xsmall:min-w-[34vw] small:min-w-[25vw]  medium:min-w-[22vw] large:min-w-[18vw] xlarge:min-w-[15vw] 2xlarge:min-w-[14vw] rounded-md "
                           src={`https://image.tmdb.org/t/p/original/${movie2.poster_path}`}
                           alt="no image available"
                           onError={(e) => {
@@ -83,7 +83,7 @@ const Upcomingdata = ({
                         />
                       </div>
                       <div className=" ">
-                        <h1 className=" text-2xl font-semibold text-yellow-500   ml-7 mt-5 ">
+                        <h1 className=" text-[5vw] xsmall:text-[3.5vw] small:text-[3vw] medium:text-[2vw] large:text-[1.7vw] xlarge:text-[1.5vw] 2xlarge:text-[1.3vw] font-semibold text-yellow-500   ml-3 mt-5 ">
                           {movie2.title}
                         </h1>
                       </div>
